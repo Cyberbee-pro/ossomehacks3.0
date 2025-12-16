3329d40988e5803a02259f01bc6929d39a192345
Sahil 2006 <sahilshabbirharyani@gmail.com>
2025-12-16 22:51:08 +0300

    almost done except 1 last layer

--- src/components/themes/demo.js
+++ src/components/themes/demo.js
@@ -41,7 +41,7 @@ const Themes = () => {
         <h2 className="text-5xl md:text-6xl font-bold text-[#FFC627] text-center mb-12 md:mb-16 tracking-wide font-poppins">
           Themes
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10 justify-items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20 md:gap-10 justify-items-center">
           {themes.map((theme) => (
             <div
               key={theme.id}
@@ -61,12 +61,12 @@ const Themes = () => {
                   alt="stamp border middle"
                   width={220}
                   height={310}
-                  className="absolute top-3 sm:top-4 md:top-5 lg:top-6 left-2 sm:left-3 md:left-6 lg:left-5 right-4 sm:right-5 md:right-2 lg:right-7 bottom-4 sm:bottom-5 md:bottom-6 lg:bottom-7 object-cover z-30"
+                  className="absolute top-4 sm:top-4 md:top-5 lg:top-6 left-4 sm:left-3 md:left-6 lg:left-5 right-0.5 sm:right-5 md:right-2 lg:right-7 bottom-0.5 sm:bottom-5 md:bottom-6 lg:bottom-7 object-cover z-30"
                 />
 
-                <div className={`absolute inset-0 flex items-start z-50 pointer-events-none ${theme.id === 5 ? 'pt-16 sm:pt-20 md:pt-12 lg:pt-8' : theme.id === 6 ? 'pt-16 sm:pt-20 md:pt-12 lg:pt-8' : 'pt-16 sm:pt-20 md:pt-12 lg:pt-12'} ${theme.id === 5 ? 'justify-center md:justify-center lg:justify-center' : theme.id === 6 ? 'justify-center md:justify-center' : 'justify-center md:justify-start lg:justify-start'} ${theme.id === 1 ? 'md:pl-18 lg:pl-[4.5rem]' : theme.id === 2 ? 'md:pl-16 lg:pl-16' : theme.id === 3 ? 'md:pl-20 lg:pl-[4.5rem]' : theme.id === 4 ? 'md:pl-12 lg:pl-10' : theme.id === 5 ? 'md:-translate-x-6 lg:-translate-x-11' : theme.id === 6 ? 'md:-translate-x-6 lg:-translate-x-12' : 'md:pl-18 lg:pl-16'}`}>
+                <div className={`absolute inset-0 flex items-start z-50 pointer-events-none ${theme.id === 5 ? 'pt-6 sm:pt-20 md:pt-12 lg:pt-12' : theme.id === 6 ? 'pt-11 sm:pt-20 md:pt-12 lg:pt-12' : 'pt-11 sm:pt-20 md:pt-12 lg:pt-12'} ${theme.id === 5 ? 'justify-center md:justify-center lg:justify-center' : theme.id === 6 ? 'justify-center md:justify-center' : 'justify-center md:justify-start lg:justify-start'} ${theme.id === 1 ? 'md:pl-18 lg:pl-[4.5rem]' : theme.id === 2 ? 'md:pl-16 lg:pl-16' : theme.id === 3 ? 'md:pl-20 lg:pl-[4.5rem]' : theme.id === 4 ? 'md:pl-12 lg:pl-10' : theme.id === 5 ? 'md:-translate-x-6 lg:-translate-x-11' : theme.id === 6 ? 'md:-translate-x-6 lg:-translate-x-12' : 'md:pl-18 lg:pl-16'}`}>
                   <span
-                    className={`text-2xl leading-tight sm:text-3xl font-semibold drop-shadow mt-2 sm:mt-1 md:-mt-2 lg:mt-0 ${theme.id === 6 ? 'md:text-3xl text-center md:text-center' : theme.id === 5 ? 'md:text-3xl text-center md:text-center lg:text-center' : 'md:text-4xl text-center md:text-left lg:text-left'}`}
+                    className={`text-4xl leading-tight sm:text-3xl font-semibold drop-shadow mt-2 sm:mt-1 md:-mt-2 lg:mt-0 ${theme.id === 6 ? 'md:text-3xl text-center md:text-center' : theme.id === 5 ? 'md:text-3xl text-center md:text-center lg:text-center' : 'md:text-4xl text-center md:text-left lg:text-left'}`}
                     style={{ color: '#ffffff' }}
                   >
                     {theme.id === 6 ? (
@@ -87,8 +87,8 @@ const Themes = () => {
                   </span>
                 </div>
 
-                <div className={`absolute inset-0 flex items-center justify-center z-40 ${theme.id === 1 ? 'md:-translate-x-4 md:-translate-y-9 lg:-translate-x-8 lg:-translate-y-10' : theme.id === 2 ? 'md:-translate-x-5 md:-translate-y-9 lg:-translate-x-8 lg:-translate-y-[2.375rem]' : theme.id === 3 ? 'md:-translate-x-5 md:-translate-y-9 lg:-translate-x-8 lg:-translate-y-[2.375rem]' : theme.id === 4 ? 'md:-translate-x-5 md:-translate-y-9 scale-110 lg:-translate-x-10 lg:-translate-y-12' : theme.id === 5 ? 'md:-translate-x-4 md:-translate-y-5 lg:-translate-x-8 lg:-translate-y-10' : theme.id === 6 ? 'md:translate-y-2 lg:translate-y-2' : ''}`}>
-                  <div className="relative w-36 h-36 sm:w-44 sm:h-44 md:w-52 md:h-52 lg:w-56 lg:h-56">
+                <div className={`absolute inset-0 flex items-center justify-center z-40 ${theme.id === 1 ? 'translate-y-2 md:-translate-x-4 md:-translate-y-9 lg:-translate-x-8 lg:-translate-y-10' : theme.id === 2 ? 'translate-y-[1.375rem] md:-translate-x-5 md:-translate-y-9 lg:-translate-x-8 lg:-translate-y-[2.375rem]' : theme.id === 3 ? 'translate-y-[1.375rem] md:-translate-x-5 md:-translate-y-9 lg:-translate-x-8 lg:-translate-y-[2.375rem]' : theme.id === 4 ? 'translate-y-2 -translate-x-1 md:-translate-x-5 md:-translate-y-9 scale-110 lg:-translate-x-10 lg:-translate-y-12' : theme.id === 5 ? 'translate-y-3 md:-translate-x-4 md:-translate-y-9 lg:-translate-x-8 lg:-translate-y-10' : theme.id === 6 ? 'md:translate-y-2 lg:translate-y-2' : ''}`}>
+                  <div className={`relative ${theme.id === 1 ? 'w-48 h-48' : theme.id === 2 ? 'w-44 h-44' : theme.id === 3 ? 'w-44 h-44' : theme.id === 4 ? 'w-52 h-52' : theme.id === 5 ? 'w-44 h-44' : theme.id === 6 ? 'w-52 h-52' : 'w-36 h-36'} sm:w-44 sm:h-44 md:w-52 md:h-52 lg:w-56 lg:h-56`}>
                     <Image
                       src={theme.image}
                       alt={theme.title}
