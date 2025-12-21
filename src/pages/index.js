b0e583086b525e439f63053171283047db612ff7
Debojyoti Ganguly <debojyotiganguly70@gmail.com>
2025-12-21 23:13:37 +0530

    fixed merge conflicts

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import ContactSection from "@/components/contactUs/contactUs";
 import MapBox from "@/components/timeLine/mapbox";
 import FAQSection from "@/components/faq/FAQSection";
 
+
 export default function Home() {
   return (
     <>
@@ -21,7 +22,12 @@ export default function Home() {
         <p className="font-medium text-navy">Medium (500)</p>
         <p className="font-semibold text-black-custom">Semi Bold (600)</p>
         <p className="font-bold text-green-custom">Bold (700)</p>
+<<<<<<< Updated upstream
       </div> */}
+=======
+      </div>
+      <FAQSection />
+>>>>>>> Stashed changes
     </>
   );
 }
